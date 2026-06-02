0537a9a9887411ff0c12a3955aab5689d531e9a3
Thomas Tesche <thtesche@gmail.com>
2026-06-02 14:20:14 +0200

    fix: correct site base path and update navigation link labels

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thtesche.github.io',
-  base: '/ashtanga-yoga-zentral',
+  base: '/ashtanga-yoga-zentral/',
 });
