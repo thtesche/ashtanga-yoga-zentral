0cd69d688404fc90fec757ff114cae173a7c57c2
Thomas Tesche <thtesche@gmail.com>
2026-06-13 09:08:20 +0200

    feature: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          de: 'de-DE',
+        },
+      },
+    }),
+  ],
   site: 'https://thtesche.github.io',
   base: '/ashtanga-yoga-zentral/',
 
